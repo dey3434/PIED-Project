ec1c4a081a2c18ea299a6a5faf4f754783a4b81d
dey3434 <48529197+dey3434@users.noreply.github.com>
2021-03-05 20:01:37 -0700

    commit 3/5/21

--- stansharmila_springfalltemp.R
+++ stansharmila_springfalltemp.R
@@ -910,9 +910,9 @@ Precip_JulAug_Tmean_SepOctint <- rbind(ci.Precip_JulAughigh.df, ci.Precip_JulAug
 ggplot(data = Precip_JulAug_Tmean_SepOctint, aes(x = Precip_JulAug, y = median, color = ci.group)) + geom_ribbon(aes(x = Precip_JulAug, ymin = ci.low, ymax = ci.high, fill = ci.group),color = NA, alpha = 0.5) + 
   geom_line() + mytheme + ylab("Predicted Growth") + ylim(0, 2)
 ggplot(data = Precip_JulAug_Tmean_SepOctint, aes(x = Precip_JulAug, y = median, color = ci.group)) +
-  scale_color_manual(values=c("#4575B4", "#FDDBC7", "#B2182B")) +
+  scale_color_manual(values=c("#B2182B", "#FDDBC7", "#4575B4")) +
   geom_ribbon(aes(x = Precip_JulAug, ymin = ci.low, ymax = ci.high, fill = ci.group), color = NA, alpha = 0.5) +
-  scale_fill_manual(values=c("#4575B4", "#FDDBC7", "#B2182B")) + geom_line() + mytheme + ylab("Predicted Growth") + ylim(0, 2)
+  scale_fill_manual(values=c("#B2182B", "#FDDBC7", "#4575B4")) + geom_line() + mytheme + ylab("Predicted Growth") + ylim(0, 2)
 
 
 #Precip_JulAug and Precip_NovDecJanFebMar
@@ -3039,9 +3039,9 @@ ggplot(data = size_PrecipDecJanFebint, aes(x = size, y = median, color = ci.grou
 #New interaction plots
 
 biggrid <- expand_grid(ppt_norm = as.vector(quantile(grow_train$ppt_norm, c(0, .2, .6, .8, 1.0))), tmp_norm = as.vector(quantile(grow_train$tmp_norm, c(0, .2, .6, .8, 1.0))),
-            Precip_NovDecJanFebMar = as.vector(quantile(grow_train$Precip_NovDecJanFebMar, c(0, .2, .6, .8, 1.0))), Precip_JulAug = as.vector(quantile(grow_train$Precip_JulAug, c(0, .2, .6, .8, 1.0))),
-            Tmean_AprMayJun = as.vector(quantile(grow_train$Tmean_AprMayJun, c(0, .2, .6, .8, 1.0))), Tmean_SepOct = as.vector(quantile(grow_train$Tmean_SepOct, c(0, .2, .6, .8, 1.0))),
-            DIA_prev = as.vector(quantile(grow_train$DIA_prev, c(0, .2, .6, .8, 1.0))))
+                       Precip_NovDecJanFebMar = as.vector(quantile(grow_train$Precip_NovDecJanFebMar, c(0, .2, .6, .8, 1.0))), Precip_JulAug = as.vector(quantile(grow_train$Precip_JulAug, c(0, .2, .6, .8, 1.0))),
+                       Tmean_AprMayJun = as.vector(quantile(grow_train$Tmean_AprMayJun, c(0, .2, .6, .8, 1.0))), Tmean_SepOct = as.vector(quantile(grow_train$Tmean_SepOct, c(0, .2, .6, .8, 1.0))),
+                       DIA_prev = as.vector(quantile(grow_train$DIA_prev, c(0, .2, .6, .8, 1.0))))
 
 ppt_norm <- biggrid$ppt_norm
 tmp_norm <- biggrid$tmp_norm
@@ -3080,35 +3080,41 @@ growthpredictioncbind <- cbind(ci.growthprediction, biggrid)
 growthpredictionfilter2 <- growthpredictioncbind
 growthpredictionfilter2$ppt_norm_character <- ifelse(growthpredictionfilter2$ppt_norm == quantile (grow_train$ppt_norm, c(0)), "lowest ppt_norm", 
                                                      ifelse(growthpredictionfilter2$ppt_norm == quantile(grow_train$ppt_norm, c(0.2)), "low ppt_norm", 
-                                                     ifelse(growthpredictionfilter2$ppt_norm == quantile(grow_train$ppt_norm, c(0.6)), "mid ppt_norm", 
-                                                     ifelse(growthpredictionfilter2$ppt_norm == quantile(grow_train$ppt_norm, c(0.8)), "high ppt_norm", "highest ppt_norm"))))
-growthpredictionfilter2$ppt_norm_character <- factor(growthpredictionfilter2$ppt_norm_character, levels = c("low ppt_norm", "mid ppt_norm", "high ppt_norm"))
-growthpredictionfilter2$tmp_norm_character <- ifelse(growthpredictionfilter2$tmp_norm == quantile(grow_train$tmp_norm, c(0.2)), "low tmp_norm", 
-                                                     ifelse(growthpredictionfilter2$tmp_norm == quantile(grow_train$tmp_norm, c(0.6)), "mid tmp_norm",
-                                                     "high tmp_norm"))
-growthpredictionfilter2$tmp_norm_character <- factor(growthpredictionfilter2$tmp_norm_character, levels = c("low tmp_norm", "mid tmp_norm", "high tmp_norm"))
-growthpredictionfilter2$Precip_NovDecJanFebMar_character <- ifelse(growthpredictionfilter2$Precip_NovDecJanFebMar == quantile(grow_train$Precip_NovDecJanFebMar, c(0.2)), "low Precip_NovDecJanFebMar", 
-                                                            ifelse(growthpredictionfilter2$Precip_NovDecJanFebMar == quantile(grow_train$Precip_NovDecJanFebMar, c(0.6)), "mid Precip_NovDecJanFebMar",
-                                                            "high Precip_NovDecJanFebMar"))
-growthpredictionfilter2$Precip_NovDecJanFebMar_character <- factor(growthpredictionfilter2$Precip_NovDecJanFebMar_character, levels = c("low Precip_NovDecJanFebMar", "mid Precip_NovDecJanFebMar", "high Precip_NovDecJanFebMar"))
-growthpredictionfilter2$Precip_JulAug_character <- ifelse(growthpredictionfilter2$Precip_JulAug == quantile(grow_train$Precip_JulAug, c(0.2)), "low Precip_JulAug", 
-                                                          ifelse(growthpredictionfilter2$Precip_JulAug == quantile(grow_train$Precip_JulAug, c(0.6)), "mid Precip_JulAug",
-                                                          "high Precip_JulAug"))
-growthpredictionfilter2$Precip_JulAug_character <- factor(growthpredictionfilter2$Precip_JulAug_character, levels = c("low Precip_JulAug", "mid Precip_JulAug", "high Precip_JulAug"))
-growthpredictionfilter2$Tmean_AprMayJun_character <- ifelse(growthpredictionfilter2$Tmean_AprMayJun == quantile(grow_train$Tmean_AprMayJun, c(0.2)), "low Tmean_AprMayJun", 
-                                                            ifelse(growthpredictionfilter2$Tmean_AprMayJun == quantile(grow_train$Tmean_AprMayJun, c(0.6)), "mid Tmean_AprMayJun",
-                                                          "high Tmean_AprMayJun"))
-growthpredictionfilter2$Tmean_AprMayJun_character <- factor(growthpredictionfilter2$Tmean_AprMayJun_character, levels = c("low Tmean_AprMayJun", "mid Tmean_AprMayJun", "high Tmean_AprMayJun"))
-growthpredictionfilter2$Tmean_SepOct_character <- ifelse(growthpredictionfilter2$Tmean_SepOct == quantile(grow_train$Tmean_SepOct, c(0.2)), "low Tmean_SepOct", 
-                                                         ifelse(growthpredictionfilter2$Tmean_SepOct == quantile(grow_train$Tmean_SepOct, c(0.6)), "mid Tmean_SepOct",
-                                                            "high Tmean_SepOct"))
-growthpredictionfilter2$Tmean_SepOct_character <- factor(growthpredictionfilter2$Tmean_SepOct_character, levels = c("low Tmean_SepOct", "mid Tmean_SepOct", "high Tmean_SepOct"))
-growthpredictionfilter2$DIA_prev_character <- ifelse(growthpredictionfilter2$DIA_prev == quantile(grow_train$DIA_prev, c(0.2)), "low DIA_prev", 
-                                                     ifelse(growthpredictionfilter2$DIA_prev == quantile(grow_train$DIA_prev, c(0.6)), "mid DIA_prev",
-                                                         "high DIA_prev"))
-growthpredictionfilter2$DIA_prev_character <- factor(growthpredictionfilter2$DIA_prev_character, levels = c("low DIA_prev", "mid DIA_prev", "high DIA_prev"))
-growthpredictionfilter3 <- growthpredictioncbind %>% filter(Tmean_AprMayJun == quantile(grow_train$Tmean_AprMayJun, c(0.6)) & Tmean_SepOct == quantile(grow_train$Tmean_SepOct, c(0.6)), 
-                                                            DIA_prev == quantile(grow_train$DIA_prev, c(0.6)))
+                                                            ifelse(growthpredictionfilter2$ppt_norm == quantile(grow_train$ppt_norm, c(0.6)), "mid ppt_norm", 
+                                                                   ifelse(growthpredictionfilter2$ppt_norm == quantile(grow_train$ppt_norm, c(0.8)), "high ppt_norm", "highest ppt_norm"))))
+growthpredictionfilter2$ppt_norm_character <- factor(growthpredictionfilter2$ppt_norm_character, levels = c("lowest ppt_norm", "low ppt_norm", "mid ppt_norm", "high ppt_norm", "highest ppt_norm"))
+growthpredictionfilter2$tmp_norm_character <- ifelse(growthpredictionfilter2$tmp_norm == quantile (grow_train$tmp_norm, c(0)), "lowest tmp_norm", 
+                                                     ifelse(growthpredictionfilter2$tmp_norm == quantile(grow_train$tmp_norm, c(0.2)), "low tmp_norm", 
+                                                            ifelse(growthpredictionfilter2$tmp_norm == quantile(grow_train$tmp_norm, c(0.6)), "mid tmp_norm", 
+                                                                   ifelse(growthpredictionfilter2$tmp_norm == quantile(grow_train$tmp_norm, c(0.8)), "high tmp_norm", "highest tmp_norm"))))
+growthpredictionfilter2$tmp_norm_character <- factor(growthpredictionfilter2$tmp_norm_character, levels = c("lowest tmp_norm", "low tmp_norm", "mid tmp_norm", "high tmp_norm", "highest tmp_norm"))
+growthpredictionfilter2$Precip_NovDecJanFebMar_character <- ifelse(growthpredictionfilter2$Precip_NovDecJanFebMar == quantile (grow_train$Precip_NovDecJanFebMar, c(0)), "lowest Precip_NovDecJanFebMar", 
+                                                                   ifelse(growthpredictionfilter2$Precip_NovDecJanFebMar == quantile(grow_train$Precip_NovDecJanFebMar, c(0.2)), "low Precip_NovDecJanFebMar", 
+                                                                          ifelse(growthpredictionfilter2$Precip_NovDecJanFebMar == quantile(grow_train$Precip_NovDecJanFebMar, c(0.6)), "mid Precip_NovDecJanFebMar", 
+                                                                                 ifelse(growthpredictionfilter2$Precip_NovDecJanFebMar == quantile(grow_train$Precip_NovDecJanFebMar, c(0.8)), "high Precip_NovDecJanFebMar", "highest Precip_NovDecJanFebMar"))))
+growthpredictionfilter2$Precip_NovDecJanFebMar_character <- factor(growthpredictionfilter2$Precip_NovDecJanFebMar_character, levels = c("lowest Precip_NovDecJanFebMar", "low Precip_NovDecJanFebMar", "mid Precip_NovDecJanFebMar", "high Precip_NovDecJanFebMar", "highest Precip_NovDecJanFebMar"))
+growthpredictionfilter2$Precip_JulAug_character <- ifelse(growthpredictionfilter2$Precip_JulAug == quantile (grow_train$Precip_JulAug, c(0)), "lowest Precip_JulAug", 
+                                                          ifelse(growthpredictionfilter2$Precip_JulAug == quantile(grow_train$Precip_JulAug, c(0.2)), "low Precip_JulAug", 
+                                                                 ifelse(growthpredictionfilter2$Precip_JulAug == quantile(grow_train$Precip_JulAug, c(0.6)), "mid Precip_JulAug", 
+                                                                        ifelse(growthpredictionfilter2$Precip_JulAug == quantile(grow_train$Precip_JulAug, c(0.8)), "high Precip_JulAug", "highest Precip_JulAug"))))
+growthpredictionfilter2$Precip_JulAug_character <- factor(growthpredictionfilter2$Precip_JulAug_character, levels = c("lowest Precip_JulAug", "low Precip_JulAug", "mid Precip_JulAug", "high Precip_JulAug", "highest Precip_JulAug"))
+growthpredictionfilter2$Tmean_AprMayJun_character <- ifelse(growthpredictionfilter2$Tmean_AprMayJun == quantile (grow_train$Tmean_AprMayJun, c(0)), "lowest Tmean_AprMayJun", 
+                                                            ifelse(growthpredictionfilter2$Tmean_AprMayJun == quantile(grow_train$Tmean_AprMayJun, c(0.2)), "low Tmean_AprMayJun", 
+                                                                   ifelse(growthpredictionfilter2$Tmean_AprMayJun == quantile(grow_train$Tmean_AprMayJun, c(0.6)), "mid Tmean_AprMayJun", 
+                                                                          ifelse(growthpredictionfilter2$Tmean_AprMayJun == quantile(grow_train$Tmean_AprMayJun, c(0.8)), "high Tmean_AprMayJun", "highest Tmean_AprMayJun"))))
+growthpredictionfilter2$Tmean_AprMayJun_character <- factor(growthpredictionfilter2$Tmean_AprMayJun_character, levels = c("lowest Tmean_AprMayJun", "low Tmean_AprMayJun", "mid Tmean_AprMayJun", "high Tmean_AprMayJun", "highest Tmean_AprMayJun"))
+growthpredictionfilter2$Tmean_SepOct_character <- ifelse(growthpredictionfilter2$Tmean_SepOct == quantile (grow_train$Tmean_SepOct, c(0)), "lowest Tmean_SepOct", 
+                                                         ifelse(growthpredictionfilter2$Tmean_SepOct == quantile(grow_train$Tmean_SepOct, c(0.2)), "low Tmean_SepOct", 
+                                                                ifelse(growthpredictionfilter2$Tmean_SepOct == quantile(grow_train$Tmean_SepOct, c(0.6)), "mid Tmean_SepOct", 
+                                                                       ifelse(growthpredictionfilter2$Tmean_SepOct == quantile(grow_train$Tmean_SepOct, c(0.8)), "high Tmean_SepOct", "highest Tmean_SepOct"))))
+growthpredictionfilter2$Tmean_SepOct_character <- factor(growthpredictionfilter2$Tmean_SepOct_character, levels = c("lowest Tmean_SepOct", "low Tmean_SepOct", "mid Tmean_SepOct", "high Tmean_SepOct", "highest Tmean_SepOct"))
+growthpredictionfilter2$DIA_prev_character <- ifelse(growthpredictionfilter2$DIA_prev == quantile (grow_train$DIA_prev, c(0)), "lowest DIA_prev", 
+                                                     ifelse(growthpredictionfilter2$DIA_prev == quantile(grow_train$DIA_prev, c(0.2)), "low DIA_prev", 
+                                                            ifelse(growthpredictionfilter2$DIA_prev == quantile(grow_train$DIA_prev, c(0.6)), "mid DIA_prev", 
+                                                                   ifelse(growthpredictionfilter2$DIA_prev == quantile(grow_train$DIA_prev, c(0.8)), "high DIA_prev", "highest DIA_prev"))))
+growthpredictionfilter2$DIA_prev_character <- factor(growthpredictionfilter2$DIA_prev_character, levels = c("lowest DIA_prev", "low DIA_prev", "mid DIA_prev", "high DIA_prev", "highest DIA_prev"))
+growthpredictionfilter3 <- growthpredictionfilter2 %>% filter(Tmean_AprMayJun == quantile(grow_train$Tmean_AprMayJun, c(0.6)) & Tmean_SepOct == quantile(grow_train$Tmean_SepOct, c(0.6)),
+                                                              DIA_prev == quantile(grow_train$DIA_prev, c(0.6)))
 ggplot(data = growthpredictionfilter3, aes(x = Precip_NovDecJanFebMar, y = `50%`, color = ppt_norm_character)) + geom_ribbon(data = growthpredictionfilter3, aes(x = Precip_NovDecJanFebMar, ymin = `2.5%`, ymax = `97.5%`, fill = ppt_norm_character),color = NA, alpha = 0.5) + 
   geom_line() + mytheme + ylab("Predicted Growth") + ylim(0, 2) + facet_grid(tmp_norm_character ~ Precip_JulAug_character)
 
