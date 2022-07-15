36cd5a6d9da3ef9324a4c77db1e697c590c48cb6
Kah5 <kellyannheilman@gmail.com>
2022-07-15 09:55:05 -0700

    updating prep code with climate extraction

--- prepFIAPresenceAbsence.R
+++ prepFIAPresenceAbsence.R
@@ -111,3 +111,99 @@ raster::plot(pres) # so now NAs mean there is no FIA data, zeros mean there is n
 # write.csv(climate_wna[8431:12645,],"./ClimateData/FIA_climateWNA3.csv",row.names=F)
 # write.csv(climate_wna[12646:16863,],"./ClimateData/FIA_climateWNA4.csv",row.names=F)
 # 
+
+# now extract climate data for those points:
+
+
+#### Use PRISM climate data to create rasters of monthly climate variables in PIED study region
+# from emily's code:https://github.com/emilylschultz/DemographicRangeModel/blob/master/Code/ClimateProcessing/historic.R
+library(raster)
+
+### PRISM download January 22, 2019
+### January 1981 through June 2018
+### (37*12) + 6 = 450 files
+
+# Search for PRISM files
+PRISM.path <-  "./PRISM_data/"
+ppt.path <-  "/Users/kah/Documents/docker_pecan/pecan/PRISM_data/PRISM_ppt_stable_4kmM2_189501_198012_bil/"
+ppt.path.new <-  "/Users/kah/Documents/docker_pecan/pecan/PRISM_data/PRISM_ppt_stable_4kmM3_198101_201910_bil/"
+
+
+pptFiles.old <- list.files(path = ppt.path, pattern = glob2rx("*ppt*.bil"), full.names = TRUE)
+pptFiles.new <- list.files(path = ppt.path.new, pattern = glob2rx("*ppt*.bil"), full.names = TRUE)
+pptFiles<- c(pptFiles.old, pptFiles.new)
+
+temp.path <-  "/Users/kah/Documents/docker_pecan/pecan/PRISM_data/PRISM_tmean_stable_4kmM3_189501_198012_bil/"
+temp.path.new <-  "/Users/kah/Documents/docker_pecan/pecan/PRISM_data/PRISM_tmean_stable_4kmM3_198101_202002_bil/"
+
+tempFiles.old <- list.files(path = temp.path, pattern = glob2rx("*tmean*.bil"), full.names = TRUE)
+tempFiles.new <- list.files(path = temp.path.new, pattern = glob2rx("*tmean*.bil"), full.names = TRUE)
+tempFiles<- c(tempFiles.old, tempFiles.new)
+
+#tmpFiles <- list.files(path = PRISM.path, pattern = glob2rx("*tmean*.bil"), full.names = TRUE)
+
+#vpdminFiles <- list.files(path = PRISM.path, pattern = glob2rx("*vpdmin*.bil"), full.names = TRUE)
+#vpdmaxFiles <- list.files(path = PRISM.path, pattern = glob2rx("*vpdmax*.bil"), full.names = TRUE)
+
+# Stack monthly data
+pptStack <- stack()
+#for (i in pptFiles[1:2]) {
+#  print(i)
+  #pptStack <- stack(pptStack, raster(i))
+#}
+
+pptStacklist <- lapply(pptFiles, function(i){stack(raster(i))})
+pptStack <- stack(pptStacklist)
+
+tmpStack <- stack()
+#for (i in tempFiles) {
+#  print(i)
+#  tmpStack <- stack(tmpStack, raster(i))
+#}
+
+tempStacklist <- lapply(tempFiles, function(i){stack(raster(i))})
+tempStack <- stack(tempStacklist)
+
+#vpdStack <- stack()
+#for (i in 1:length(vpdmaxFiles)) {
+#  print(i)
+#  rast<-raster(vpdmaxFiles[i])
+#  if(i == 431 | i == 432){crs(rast)<-crs(raster(vpdmaxFiles[1]))}
+#  vpdStack <- stack(vpdStack, rast)
+#}
+
+
+# extract by plot lat and lon:
+PApied <- raster("data/presenceAbsenceRasterNA.tif")
+plot(PApied)
+
+crs(pptStack)
+crs(PApied)
+
+PApied <- projectRaster(PApied, crs = crs(pptStack))
+
+FIA_PApied <- as.data.frame(PApied,xy=TRUE)
+names(FIA_PApied )<-c("lon","lat","PApied")
+
+ppt.extracted <- data.frame(extract(pptStack, FIA_PApied[,c("lon", "lat")]))
+ppt.extracted$lon <- FIA_PApied$lon
+ppt.extracted$lat <- FIA_PApied$lat
+ppt.extracted$name<- FIA_PApied$name
+
+
+saveRDS(ppt.extracted, "data/pied_extracted.ppt.PAdata.rds")
+
+ppt.extracted <- readRDS( "data/pied_extracted.ppt.PAdata.rds")
+
+
+temp.extracted <- data.frame(raster::extract(tempStack, FIA_PApied[,c("lon", "lat")]))
+temp.extracted$lon <- FIA_PApied$lon
+temp.extracted$lat <- FIA_PApied$lat
+temp.extracted$name<- FIA_PApied$name
+
+
+saveRDS(temp.extracted, "data/pied_extracted.temp.PAdata.rds")
+temp.extracted <- readRDS( "data/pied_extracted.temp.data.rds")
+
+
+
